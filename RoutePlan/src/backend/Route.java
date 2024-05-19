605475addebb6a9fe467dc2f18152fc23dd4c3ea
animogok <sebastian.avenda32@gmail.com>
2024-05-18 19:02:48 -0500

    Se agregan algunas imagenes las cuales corresponden a las rutas establecidas para la informacion a los usuarios

--- RoutePlan/src/backend/Route.java
+++ RoutePlan/src/backend/Route.java
@@ -1,10 +1,58 @@
 package backend;
 
+
+
 public class Route {
-    private String vehicle1 = "Personal car";
-    private String vehicle2 = "Passenger bus";
-    private String vehicle3 = "Plane";
-    private String vehicle4 = "Motorbike";
-    private String vehicle5 = "Plane";
-    private String vehicle6 = "Bike";
+
+    private String[] vehicles = {"Plane", "Car", "Motorbike", "Passenger bus", "Heavy vehicles", "Bike"};
+    private String[] cities = {"Bogotá", "Ibagué", "Armenia", "Pereira", "Manizales", "Medellín", "Tunja"};
+    private String vehicle = "";
+    private String city1 = "";
+    private String city2 = "";
+    private String city3 = "";
+
+    public Route(int IdVehicle, int IdCity1, int IdCity2){
+        this.vehicle = vehicles[IdVehicle];
+        this.city1 = cities[IdCity1];
+        this.city2 = cities[IdCity2];
+    }
+
+    public Route(int IdVehicle, int IdCity1, int IdCity2, int Idcity3){
+        this.vehicle = vehicles[IdVehicle];
+        this.city1 = cities[IdCity1];
+        this.city2 = cities[IdCity2];
+        this.city3 = cities[Idcity3];
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public String getCity1() {
+        return city1;
+    }
+
+    public void setCity1(String city1) {
+        this.city1 = city1;
+    }
+
+    public String getCity2() {
+        return city2;
+    }
+
+    public void setCity2(String city2) {
+        this.city2 = city2;
+    }
+
+    public String getCity3() {
+        return city3;
+    }
+
+    public void setCity3(String city3) {
+        this.city3 = city3;
+    }
 }
--- RoutePlan/src/backend/RouteInformation.java
+++ RoutePlan/src/backend/RouteInformation.java
@@ -1,5 +1,16 @@
 package backend;
 
-public class RouteInformation {
+public class RouteInformation extends Route{
+
+    private int average_vehicleSpeed = 60;
+
+    public RouteInformation(int IdVehicle, int IdCity1, int IdCity2) {
+        super(IdVehicle, IdCity1, IdCity2);
+    }
+
+    public RouteInformation(int IdVehicle, int IdCity1, int IdCity2, int Idcity3) {
+        super(IdVehicle, IdCity1, IdCity2, Idcity3);
+    }
+    
     
 }
